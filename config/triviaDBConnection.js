d1fa718f242652c076f4103ae05c8ee13f5dd338
Eduardo Galeano <cregaleanoac@unal.edu.co>
2018-10-04 15:57:39 -0500

    Fixed error on connections files.

--- config/triviaDBConnection.js
+++ config/triviaDBConnection.js
@@ -6,4 +6,4 @@ const knexFile = require('./triviaDBParameters');
 const knexConfig = Knex(); //specify environment (knexFile.development or knexFile.production)
 Model.knex(knexConfig);
 
-module.exports(Model);
+module.exports = Model;
--- config/wordpressDBConnection.js
+++ config/wordpressDBConnection.js
@@ -6,4 +6,4 @@ const knexFile = require('./wordpressDBParameters');
 const knexConfig = Knex(); //specify environment (knexFile.development or knexFile.production)
 Model.knex(knexConfig);
 
-module.exports(Model);
+module.exports = Model;
